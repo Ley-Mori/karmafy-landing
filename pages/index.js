618de057fcadafe1de89904cf0284f5bdb5401ff
Ley-Mori <elia.angotzi@proton.me>
2025-05-02 18:26:26 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+
+export default function HomePage() {
+  return (
+    <main className="min-h-screen bg-white text-gray-800 px-6 py-10 font-sans">
+      <section className="text-center mb-16">
+        <h1 className="text-4xl font-bold mb-4">KARMAFY</h1>
+        <p className="text-xl">Where Karma Becomes Token</p>
+      </section>
+    </main>
+  );
+}
